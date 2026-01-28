5b71915a4baa0f959e3ac2b9bbc5e5c2d68c930f
Affan Shaikh <affansshaikh76@gmail.com>
2026-01-28 07:36:36 +0000

    Understanding Basics

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,9 @@
+let name = "Affan"
+let age = 20
+let isLoggedIn = true
+let temp = null;
+console.log(typeof(name) );
+console.log(typeof(age) );
+console.log(typeof(isLoggedIn) );
+console.log(typeof(temp) ); // null -> object
+console.log(typeof(undefined) ); //undefined -> undefined
