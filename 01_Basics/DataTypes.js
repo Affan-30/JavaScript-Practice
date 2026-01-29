3ad359f06e435587ca02f3373702de8bf4fc42fe
Affan Shaikh <affansshaikh76@gmail.com>
2026-01-29 22:42:18 +0530

    Delete directory

--- 01_Basics/DataTypes.js
+++
@@ -1,9 +0,0 @@
-let name = "Affan"
-let age = 20
-let isLoggedIn = true
-let temp = null;
-console.log(typeof(name) );
-console.log(typeof(age) );
-console.log(typeof(isLoggedIn) );
-console.log(typeof(temp) ); // null -> object
-console.log(typeof(undefined) ); //undefined -> undefined
--- 01_Basics/Variables.js
+++
@@ -1,8 +0,0 @@
-const accID = 12345; //Can not be changed
-let accName = "John Doe"; // maintain block scope
-var accEmail = "john.doe@example.com"; // does not maintain block scope so prefer not to use
-accCity = "New York"; // without using let,var,canst the variable creation in jS is possible (Not Recommended)
-let accState;
-console.log(accID);
-
-console.table([accID, accName, accEmail, accCity, accState])
