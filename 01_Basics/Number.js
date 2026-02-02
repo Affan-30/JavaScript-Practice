6b24e303d5f1e91ad59a54d925cda31f089e99df
Affan-30 <affansshaikh76@gmail.com>
2026-02-02 16:38:11 +0530

    Numbers and Maths in JS

---
+++ 01_Basics/Number.js
@@ -0,0 +1,31 @@
+const num = 1907;
+
+console.log(num);
+
+const no = new Number(206)
+console.log(no);
+
+const f = 38.8195
+console.log(f.toPrecision(4));
+
+console.log(f.toFixed(1));
+
+const hun = 1000000
+console.log(hun.toLocaleString('en-IN'));
+
+console.log(Math);
+
+console.log(Math.abs(-9));
+console.log(Math.round(5.4));
+console.log(Math.ceil(5.4));
+console.log(Math.floor(5.4));
+
+Math.random()
+console.log(Math.floor((Math.random()*10)+1));
+
+const min = 50
+const max = 60
+
+console.log(Math.floor( Math.random() * (max-min + 1)) + min);
+
+
