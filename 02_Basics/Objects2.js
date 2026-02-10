92ab2ad78fbaadd04916220cc6977e7a69a6fbe9
Affan-30 <affansshaikh76@gmail.com>
2026-02-10 17:06:52 +0530

    More of Objects

---
+++ 02_Basics/Objects2.js
@@ -0,0 +1,39 @@
+// const obj1 = new Object(); //singleton object
+
+const obj1 = {} // non singleton object
+
+obj1.id = 38
+obj1.name = "Affan"
+obj1.isLoggedIn = true
+
+
+const common = {
+    email : "some@mail.com",
+    fullname : {
+        firstname : "Affan",
+        middlename : "Sarfaraz",
+        lastname : "Shaikh"
+    }
+}
+
+
+obj1.info = common
+// console.log(common.fullname.lastname);
+// console.log(obj1.info.fullname.middlename);
+
+const o1 = {1:'a', 2:'b'}
+const o2 = {3:'c', 4: 'd'}
+
+// const o3 = Object.assign({}, o1, o2)
+
+const o3 = {...o1, ...o2}
+
+// console.log(o3);
+
+console.log(obj1);
+// console.log(Object.keys(obj1));
+// console.log(Object.values(obj1));
+// console.log(Object.entries(obj1));
+
+console.log(obj1.hasOwnProperty("id2"));
+
