aaf2e23c72f40356a6ac222d58f42e51e96ed1ba
Affan-30 <affansshaikh76@gmail.com>
2026-02-19 16:10:03 +0530

    More of Functions

--- 03_Basics/functions.js
+++ 03_Basics/functions.js
@@ -16,3 +16,36 @@ function greetings(username = "affan"){
 
 // const greet = greetings()
 console.log(greetings("Tohid murshad"));
+
+//Functions where input no. is not fixed
+
+function calculateCartPrice(...num){
+return num;
+}
+
+console.log(calculateCartPrice(100,200,300,400));
+
+const user = {
+    username: "affan",
+    price : 199
+}
+
+function handleObject(anyObj){
+    console.log(`Current user is ${anyObj.username} and price is ${anyObj.price}.`);
+    
+}
+
+handleObject(user);
+handleObject({
+    username: "sam",
+    price : 399
+})
+
+const arr = [1,2,3,4,5]
+
+function add1last (arr) {
+    return arr[0] + arr[arr.length-1];
+}
+
+console.log(add1last(arr))
+console.log(add1last([1,2,3,1]))
