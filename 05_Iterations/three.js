f7ccacbeceeca9440b7e1f35f2a74b3733e4880a
Affan-30 <affansshaikh76@gmail.com>
2026-03-03 22:50:05 +0530

    for loops

---
+++ 05_Iterations/three.js
@@ -0,0 +1,14 @@
+
+const array = [1,2,3,4,5
+
+]
+for (const num of array) {
+    // console.log(num);
+    
+}
+
+const line = "hello world!"
+for (const num of line) {
+    console.log(num);
+    
+}
