8851d91d4cdd51a1ef4adf8121f34f5932f820e6
Affan-30 <affansshaikh76@gmail.com>
2026-03-04 23:02:38 +0530

    for loops - 1

--- 05_Iterations/three.js
+++ 05_Iterations/three.js
@@ -1,7 +1,6 @@
 
-const array = [1,2,3,4,5
-
-]
+const array = [1,2,3,4,5]
+//      (variable) (item to iterate)
 for (const num of array) {
     // console.log(num);
     
@@ -9,6 +8,22 @@ for (const num of array) {
 
 const line = "hello world!"
 for (const num of line) {
-    console.log(num);
+    // console.log(num);
     
+}
+
+const  map = new Map()
+
+map.set("UAE","United Arab Emirates")
+map.set("SAR","Kingdom of Saudi Arabia")
+map.set("IN","Republic of India")
+
+// console.log(map);
+
+for (const key of map) {
+    // console.log(key); // array of key - value pair
+}
+
+for (const [key, value] of map) {
+    console.log(key, " : ", value);
 }
